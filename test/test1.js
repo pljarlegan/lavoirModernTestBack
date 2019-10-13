530e40de8da497ebda36d4e1dfccfb1bda361401
pljarlegan <pl.jarlegan@citae.fr>
2019-10-13 17:58:43 +0200

    MIN: reorder tests ...

--- test/test1.js
+++ test/test1.js
@@ -3,19 +3,7 @@ const expect = require("chai").expect,
   api = supertest("http://localhost:3000");
 
 
-describe("GET localhost:3000/test1/recursion", () => {
-  "use strict";
-  it("should return a 200 response, with empty collection", (done) => {
-    api.post("/test1/recursion")
-      .set("Accept", "application/json")
-      .send({ list: [ 1, 2, 3 ] })
-      .end((error, response) => {
-        expect(response.body).to.deep.equal({ result: 6 });
-        expect(200, done);
-        done();
-      });
-  });
-});
+
 describe("GET localhost:3000/test1/for", () => {
   "use strict";
   it("should return a 200 response, with empty collection", (done) => {
@@ -46,6 +34,19 @@ describe("GET localhost:3000/test1/while", () => {
   "use strict";
   it("should return a 200 response, with empty collection", (done) => {
     api.post("/test1/while")
+      .set("Accept", "application/json")
+      .send({ list: [ 1, 2, 3 ] })
+      .end((error, response) => {
+        expect(response.body).to.deep.equal({ result: 6 });
+        expect(200, done);
+        done();
+      });
+  });
+});
+describe("GET localhost:3000/test1/recursion", () => {
+  "use strict";
+  it("should return a 200 response, with empty collection", (done) => {
+    api.post("/test1/recursion")
       .set("Accept", "application/json")
       .send({ list: [ 1, 2, 3 ] })
       .end((error, response) => {
