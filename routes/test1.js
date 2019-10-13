b0a709e28318ad04a3bf6a5a7ef9e797c6005afa
pljarlegan <pl.jarlegan@citae.fr>
2019-10-13 18:10:29 +0200

    MOD: refacto

--- routes/test1.js
+++ routes/test1.js
@@ -1,5 +1,36 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(),
+  calcFor = (list) => {
+    let result = 0;
+    for (let i = 0; i < list.length ; i++) {
+      result += list[i];
+    }
+    return result;
+  },
+  calcForEach = (list) => {
+    let result = 0;
+    list.forEach((item) => result += item);
+    return result;
+  },
+  calcWhile = (list) => {
+    let result = 0;
+    while (list.length > 0) {
+      result += list.pop();
+      if (list.length === 0) {
+        return result;
+      }
+    }
+  },
+  calcRecursion = (list) => {
+    const calc = (items, result) => {
+      result += items.pop();
+      if (items.length > 0) {
+        return calc(items, result);
+      }
+      return result;
+    };
+    return calc(list, 0);
+  };
 
 router.get("/", (req, res) => {
   "use strict";
@@ -8,44 +39,22 @@ router.get("/", (req, res) => {
 
 router.post("/for", (req, res) => {
   "use strict";
-  let result = 0;
-  for (let i = 0; i < req.body.list.length ; i++) {
-    result += req.body.list[i];
-  }
-  res.json({ result });
+  res.json({ result: calcFor(req.body.list) });
 });
 
 router.post("/foreach", (req, res) => {
   "use strict";
-  let result = 0;
-  req.body.list.forEach((item) => result += item);
-  res.json({ result });
+  res.json({ result: calcForEach(req.body.list) });
 });
 
 router.post("/while", (req, res) => {
   "use strict";
-  let result = 0;
-  const list = req.body.list;
-
-  while (list.length > 0) {
-    result += list.pop();
-    if (list.length === 0) {
-      res.json({ result });
-    }
-  }
+  res.json({ result: calcWhile(req.body.list) });
 });
 
 router.post("/recursion", (req, res) => {
   "use strict";
-  const list = req.body.list,
-    calc = (items, result) => {
-      result += items.pop();
-      if (items.length > 0) {
-        return calc(items, result);
-      }
-      return result;
-    };
-  res.json({ result: calc(list, 0) });
+  res.json({ result: calcRecursion(req.body.list) });
 });
 
 module.exports = router;
