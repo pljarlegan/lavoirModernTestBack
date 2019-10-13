175bd2c834ee8dcaa0b99688787736175e63a645
pljarlegan <pl.jarlegan@citae.fr>
2019-10-13 17:43:19 +0200

    ADD: test file for test1

---
+++ test/test1.js
@@ -0,0 +1,57 @@
+const expect = require("chai").expect,
+  supertest = require("supertest"),
+  api = supertest("http://localhost:3000");
+
+
+describe("GET localhost:3000/test1/recursion", () => {
+  "use strict";
+  it("should return a 200 response, with empty collection", (done) => {
+    api.post("/test1/recursion")
+      .set("Accept", "application/json")
+      .send({ list: [ 1, 2, 3 ] })
+      .end((error, response) => {
+        expect(response.body).to.deep.equal({ result: 6 });
+        expect(200, done);
+        done();
+      });
+  });
+});
+describe("GET localhost:3000/test1/for", () => {
+  "use strict";
+  it("should return a 200 response, with empty collection", (done) => {
+    api.post("/test1/for")
+      .set("Accept", "application/json")
+      .send({ list: [ 1, 2, 3 ] })
+      .end((error, response) => {
+        expect(response.body).to.deep.equal({ result: 6 });
+        expect(200, done);
+        done();
+      });
+  });
+});
+describe("GET localhost:3000/test1/foreach", () => {
+  "use strict";
+  it("should return a 200 response, with empty collection", (done) => {
+    api.post("/test1/foreach")
+      .set("Accept", "application/json")
+      .send({ list: [ 1, 2, 3 ] })
+      .end((error, response) => {
+        expect(response.body).to.deep.equal({ result: 6 });
+        expect(200, done);
+        done();
+      });
+  });
+});
+describe("GET localhost:3000/test1/while", () => {
+  "use strict";
+  it("should return a 200 response, with empty collection", (done) => {
+    api.post("/test1/while")
+      .set("Accept", "application/json")
+      .send({ list: [ 1, 2, 3 ] })
+      .end((error, response) => {
+        expect(response.body).to.deep.equal({ result: 6 });
+        expect(200, done);
+        done();
+      });
+  });
+});
